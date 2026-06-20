a27c4c5d41d5619a02596e56e83a04fce6ed8c14
ihriyad <ihriyad.dev@gmail.com>
2026-06-20 17:22:20 +0600

    get users, companies via get api

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const express = require("express");
 const app = express();
 require("dotenv").config();
@@ -25,11 +28,61 @@ async function run() {
 
     const database = client.db("NexRole");
     const jobCollection = database.collection("jobs");
+    const companyCollection = database.collection("companies");
+    const usersCollection = database.collection("user");
 
+    //find user
+    app.get("/api/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //post job
     app.post("/api/jobs", async (req, res) => {
       const job = req.body;
       const result = await jobCollection.insertOne(job);
       res.send(result);
+    });
+    //get job
+    app.get("/api/jobs", async (req, res) => {
+      const query = {};
+      if (req.query.companyId) {
+        query.companyId = req.query.companyId;
+      }
+      if (req.query.status) {
+        query.status = req.query.status;
+      }
+      const cursor = jobCollection.find(query)
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    // get all company
+
+    app.get("/api/companies", async(req, res)=>{
+      const cursor = companyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result || {});
+    })
+    //post company
+
+
+    app.post("/api/company", async (req, res) => {
+      const company = req.body;
+      const result = await companyCollection.insertOne(company);
+      res.send(result);
+    });
+    // get my company
+
+    app.get("/api/my/companies", async (req, res) => {
+      const query = {};
+      if (req.query.recruiterId) {
+        query.recruiterId = req.query.recruiterId;
+      }
+      const result = await companyCollection.findOne(query);
+      res.send(result || {});
     });
 
     await client.db("admin").command({ ping: 1 });
